1440ac26e3176f296d6ec1710fcce9abd234292c
keitha13 <keitha9452@gmail.com>
2021-11-13 15:54:47 +0900

    first commit

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function squareOf(n) {
+    return console.log(n * n);
+}
+squareOf(2);
+// squareOf('n')
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+function squareOf(n: number){
+  return console.log(n*n)
+}
+squareOf(2)
